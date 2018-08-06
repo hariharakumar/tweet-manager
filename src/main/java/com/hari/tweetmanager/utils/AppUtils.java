7feca2891f72d20abadf7a877732ecb98a423b4f
Hari Earlapati <harihara.earlapati@gmail.com>
2018-08-05 22:25:33 -0500

    Added utils class

--- src/main/java/com/hari/tweetmanager/utils/AppUtils.java
+++ src/main/java/com/hari/tweetmanager/utils/AppUtils.java
@@ -1,4 +1,28 @@
 package com.hari.tweetmanager.utils;
 
+import org.apache.log4j.Logger;;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class AppUtils {
+
+    static Logger logger = Logger.getLogger(AppUtils.class);
+
+    public String get32ByteRandomString() {
+        byte[] bytes = new byte[32];
+
+        try {
+            SecureRandom.getInstanceStrong().nextBytes(bytes);
+        }
+        catch (NoSuchAlgorithmException nse) {
+            logger.error("Unable to build Random String : " + nse.getMessage());
+        }
+
+        return bytes.toString();
+    }
+
+    public Long getCurrentTimeInSecondsInEpoch() {
+        return System.currentTimeMillis() / 1000L;
+    }
 }
