104de04c7c13ce0aabf0f73bdc58a8792d9ed3c1
Hari Earlapati <harihara.earlapati@gmail.com>
2018-08-05 20:08:27 -0500

    Pushing to personal repo

---
+++ src/main/java/com/hari/tweetmanager/TweetManagerApplication.java
@@ -0,0 +1,12 @@
+package com.hari.tweetmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetManagerApplication.class, args);
+	}
+}
