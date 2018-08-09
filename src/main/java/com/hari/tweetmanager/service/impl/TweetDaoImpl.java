fada18f4bbf716c95f4ac2107272c2b5b157bc22
Hari Earlapati <harihara.earlapati@gmail.com>
2018-08-09 08:42:14 -0500

    Working get request to twitter API

--- src/main/java/com/hari/tweetmanager/service/impl/TweetDaoImpl.java
+++ src/main/java/com/hari/tweetmanager/service/impl/TweetDaoImpl.java
@@ -2,10 +2,16 @@ package com.hari.tweetmanager.service.impl;
 
 import com.hari.tweetmanager.service.AuthDao;
 import com.hari.tweetmanager.service.TweetDao;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.ws.rs.core.UriBuilder;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLEncoder;
 import java.util.HashMap;
 
 @Service
@@ -26,7 +32,24 @@ public class TweetDaoImpl implements TweetDao {
         queryParams.put("count","5");
         String authHeader = authDao.getAuthHeader(httpMethod, requestBaseUrl, queryParams);
 
-        // Use the authHeader to make the actual request
+        Client client = Client.create();
+        URI baseURI = UriBuilder.fromUri(requestBaseUrl).build();
+        WebResource webResource = client.resource(baseURI);
+
+        try {
+
+            String encodedQueryParams = URLEncoder.encode("count=5", "UTF-8");
+
+            String response = webResource.queryParam("count","5")
+                              .header("Authorization", authHeader)
+                              .header("User-Agent","OAuth gem v0.4.4")
+                              .header("Host","api.twitter.com")
+                              .get(String.class);
+            System.out.println("Response from Twitter API : " + response);
+        }
+        catch (UnsupportedEncodingException use) {
+            logger.error("Unsupported encoding : " + use.getMessage());
+        }
 
 
     }
